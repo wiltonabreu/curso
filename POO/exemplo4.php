78f54716764c05a57acce45db1bb9232ae7a49d3
wiltonabreu <wiltonabreuu@gmail.com>
2018-09-04 23:06:54 -0300

    Exemplos de OO

---
+++ POO/exemplo4.php
@@ -0,0 +1,26 @@
+<?php
+
+class Endereco{
+
+	private $logradouro;
+	private $numero;
+	private $cidade;
+
+	public function __construct($logradouro,$numero,$cidade){
+		$this->logradouro = $logradouro;
+		$this->numero = $numero;
+		$this->cidade = $cidade;
+	}	
+
+	public function __toString(){
+		return $this->logradouro .", ".$this->numero ." - ". $this->cidade;
+	}
+
+
+	
+}
+
+$meuEndereco = new Endereco("Rua cava", "1234", "Santos");
+//var_dump($meuEndereco);
+echo $meuEndereco;	
+?>
